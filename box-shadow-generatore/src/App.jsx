a02eec6e2935faa9ff5543e906cd54c55823b79a
FarooqProProgrammer <107787520+FarooqProProgrammer@users.noreply.github.com>
2022-12-18 14:09:04 +0500

    begginer Project

---
+++ box-shadow-generatore/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <div className="controls">
+          <input type="range" name="" id="" />
+        </div>
+        <div className="output"></div>
+    </div>
+  );
+}
+
+export default App;
