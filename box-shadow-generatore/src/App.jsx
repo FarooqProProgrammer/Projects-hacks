a8b59259c19c2d26477da97a4e338cb45a2ca8ca
FarooqProProgrammer <107787520+FarooqProProgrammer@users.noreply.github.com>
2022-12-18 14:10:51 +0500

    begin

--- box-shadow-generatore/src/App.jsx
+++ box-shadow-generatore/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
-
+import {useState} from "react"
 function App() {
+  const [Hori,setHori] = useState();
   return (
     <div className="App">
         <div className="controls">
-          <input type="range" name="" id="" />
+          <label>Horizontal length</label>
+          <input type="range" min="-200" max="200"  name="" id="" />
         </div>
         <div className="output"></div>
     </div>
